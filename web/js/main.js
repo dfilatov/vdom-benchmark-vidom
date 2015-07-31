82890aeb2b8d5d3b8d2d4fa2097f70273534f2e5
dfilatov <dfilatov@yandex-team.ru>
2015-07-31 20:07:48 +0300

    bump to 0.0.18

--- web/js/main.js
+++ web/js/main.js
@@ -4,7 +4,7 @@ var benchmark = require('vdom-benchmark-base');
 var vidom = require('vidom');
 
 var NAME = 'vidom';
-var VERSION = '0.0.16';
+var VERSION = '0.0.18';
 
 function renderTree(nodes) {
   var children = [];
