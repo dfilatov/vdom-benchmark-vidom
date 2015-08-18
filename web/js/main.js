97f47616c9510465c8e43576352344eef4255769
dfilatov <dfilatov@yandex-team.ru>
2015-08-18 19:19:33 +0300

    bump vidom to 0.0.33

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,9 @@
 var benchmark = require('vdom-benchmark-base');
 var vidom = require('vidom');
+var node = vidom.node;
 
 var NAME = 'vidom';
-var VERSION = '0.0.27';
+var VERSION = '0.0.33';
 
 function renderTree(nodes) {
   var children = [];
@@ -12,9 +13,9 @@ function renderTree(nodes) {
   for (i = 0; i < nodes.length; i++) {
     n = nodes[i];
     if (n.children !== null) {
-      children.push(vidom.createNode('div').key(n.key).children(renderTree(n.children)));
+      children.push(node('div').key(n.key).children(renderTree(n.children)));
     } else {
-      children.push(vidom.createNode('span').key(n.key).children(n.key.toString()));
+      children.push(node('span').key(n.key).children(n.key.toString()));
     }
   }
 
@@ -37,13 +38,13 @@ BenchmarkImpl.prototype.tearDown = function() {
 BenchmarkImpl.prototype.render = function() {
   vidom.mountToDomSync(
     this.container,
-    vidom.createNode('div').children(renderTree(this.a)));
+    node('div').children(renderTree(this.a)));
 };
 
 BenchmarkImpl.prototype.update = function() {
   vidom.mountToDomSync(
     this.container,
-    vidom.createNode('div').children(renderTree(this.b)));
+    node('div').children(renderTree(this.b)));
 };
 
 document.addEventListener('DOMContentLoaded', function(e) {
